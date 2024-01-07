b52bb74862b169af5494ffe1840aaec4e545453b
do_it_tomorrow <553075930@qq.com>
2024-01-07 12:23:22 +0800

    update from gridea: 2024-01-07 12:23:22

--- media/js/index.js
+++ media/js/index.js
@@ -1,13 +1,49 @@
-/**
- * Created by 95751 on 2018/8/3.
- */
-//获取高度赋值
-var height=document.documentElement.clientHeight;
-$('#centerbg').css('height',height+'px');
-
-//判断是否为ios系统
-var u = navigator.userAgent;
-var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-if(isiOS==true){
-    $('#centerbg').css('background-attachment','scroll');
+    function init() {
+        menu = $('.menu');
+        side = $('.side');
+        main = $('#main');
+        postinfo = $('.postinfo');
+        postimage = $('.postimage');
+        tab1 = document.querySelector("#tab1");
+        musicbtn = $(".musicbtn");
+        ClicksideState = 0;
+        maindom = document.querySelector('#main')
+        mainstyle = window.getComputedStyle(maindom, null)
+        mainleft = mainstyle.left;
+        posttitle = $('.posttitle');
+        posttitle2 = $('.posttitlewithoutimg');
+    }
+    init();
+    function menu_openclose1() {
+        if (ClicksideState == 0) {
+            side.animate({
+                'left': 0
+            });
+            main.animate({
+                'width':"58.33333333%",
+                'margin-left':"25%"
+            })
+            musicbtn.fadeIn(300);
+            ClicksideState = 1;
+        } else {
+            side.animate({
+                'left': -1200
+            });
+            main.animate({
+                'width':"60vw",
+                'margin-left':"20vw"
+            })
+            musicbtn.fadeOut(100);
+            ClicksideState = 0;
+        }
+        (tab1.className == 'tab') ?
+        tab1.className = 'tab active':
+            tab1.className = 'tab';
+    }
+    
+    if(!/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)){
+    tab1.addEventListener('click', menu_openclose1);
+    menu_openclose1();
 }
+
+    
